88b887b8f7d5f5e4453e612a723a4d32c4252f55
Juan <jcasasz@pucp.edu.pe>
2021-10-12 16:03:28 -0500

    Inicializar proyecto

---
+++ src/main/java/com/back/route4d/Route4dApplication.java
@@ -0,0 +1,13 @@
+package com.back.route4d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Route4dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Route4dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/route4d/Route4dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.route4d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Route4dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
