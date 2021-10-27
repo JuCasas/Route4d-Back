8923518b1bb174a2882e9498273b59cf5e10190c
Juan <jcasasz@pucp.edu.pe>
2021-10-27 16:55:47 -0500

    Cross port 3000 origin

--- src/main/java/com/back/route4d/controller/UsuarioController.java
+++ src/main/java/com/back/route4d/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
