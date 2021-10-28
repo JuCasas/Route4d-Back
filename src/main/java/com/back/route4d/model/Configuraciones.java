aa58aec230b7f8369dbf60633ee07e40c093bb28
Rodrigo Alonso <52440153+RodCross@users.noreply.github.com>
2021-10-28 01:49:12 -0500

    Agregando configuraciones

---
+++ src/main/java/com/back/route4d/model/Configuraciones.java
@@ -0,0 +1,11 @@
+package com.back.route4d.model;
+
+public class Configuraciones {
+    public static int almacenX = 12; // coordenada X del almacén
+    public static int almacenY = 8; // coordenada Y del almacén
+    public static int almacen = almacenX + almacenY * 71 + 1; // id del nodo del almacén
+    public static int V = 3621; // cantidad de vértices del grafo
+    public static int E = 14240; // cantidad de aristas del grafo
+    public static double penalidad = 20.0;
+    public static double precio = 200.0;
+}
