5d6b91e7a4b441ddc6b78655a87914ab91ca3c83
Juan <jcasasz@pucp.edu.pe>
2021-11-02 13:26:19 -0500

    Bloqueado clase

--- src/main/java/com/back/route4d/Route4dApplication.java
+++ src/main/java/com/back/route4d/Route4dApplication.java
@@ -1,5 +1,6 @@
 package com.back.route4d;
 
+import com.back.route4d.algoritmo.Algoritmo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Route4dApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Route4dApplication.class, args);
+//		SpringApplication.run(Route4dApplication.class, args);
+		Algoritmo algoritmo = new Algoritmo();
+		algoritmo.inicializar();
+		algoritmo.generarRutas();
 	}
 
 }
