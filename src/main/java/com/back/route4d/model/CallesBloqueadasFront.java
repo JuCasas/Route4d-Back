1a04927bdb944279dbef84da6aaf0d3f93119f0b
Juan <jcasasz@pucp.edu.pe>
2021-11-11 21:02:31 -0500

    CallesBloqueadsasFront fix

--- src/main/java/com/back/route4d/model/CallesBloqueadasFront.java
+++ src/main/java/com/back/route4d/model/CallesBloqueadasFront.java
@@ -2,19 +2,22 @@ package com.back.route4d.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class CallesBloqueadasFront {
-    private final Integer minutosInicio;
-    private final Integer minutosFin;
+    private final LocalDateTime fechaInicio;
+    private final LocalDateTime fechaFin;
+    private final Integer duracionMinutos;
     public List<Map<String,Integer>> nodos;
 
-    public CallesBloqueadasFront(int minutosInicio, int minutosFin) {
-        this.minutosInicio = minutosInicio;
-        this.minutosFin = minutosFin;
+    public CallesBloqueadasFront(LocalDateTime fechaInicio, LocalDateTime fechaFin, Integer duracionMinutos) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.duracionMinutos = duracionMinutos;
         this.nodos = new ArrayList<> ();
     }
 
