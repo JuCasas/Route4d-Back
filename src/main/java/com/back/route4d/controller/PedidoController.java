f9b39533fd947fe7464d3ce95671c6500f5cecd9
Juan <jcasasz@pucp.edu.pe>
2021-11-24 11:44:38 -0500

    Fix pedido reporte

--- src/main/java/com/back/route4d/controller/PedidoController.java
+++ src/main/java/com/back/route4d/controller/PedidoController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -56,7 +53,7 @@ public class PedidoController {
     }
 
     @PostMapping("/filterDate")
-    public HashMap filterByDate(@RequestBody Map<String, String> json){
+    public List<Map<String, Object>> filterByDate(@RequestBody Map<String, String> json){
         //String inicio = "2016-03-04 11:30";
         String inicio = json.get("inicio");
         String fin = json.get("fin");
@@ -67,64 +64,23 @@ public class PedidoController {
 
         List<Pedido> lista = pedidoService.findByDate(dateTimeInicio,dateTimeFin);
 
-        HashMap <String, Integer> meses = new LinkedHashMap<>();
+        List<Map<String,Object>> data = new ArrayList<>();
+        List<String> mesesNombre = Arrays.asList("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 
-        meses.put("Enero",0);
-        meses.put("Febrero",0);
-        meses.put("Marzo",0);
-        meses.put("Abril",0);
-        meses.put("Mayo",0);
-        meses.put("Junio",0);
-        meses.put("Julio",0);
-        meses.put("Agosto",0);
-        meses.put("Septiembre",0);
-        meses.put("Octubre",0);
-        meses.put("Noviembre",0);
-        meses.put("Diciembre",0);
-
-        for (Pedido pedido:lista){
-            int mes = pedido.getFechaPedido().getMonthValue();
-            switch (mes){
-                case 1:
-                    meses.put("Enero", meses.get("Enero") + 1);
-                    break;
-                case 2:
-                    meses.put("Febrero", meses.get("Febrero") + 1);
-                    break;
-                case 3:
-                    meses.put("Marzo", meses.get("Marzo") + 1);
-                    break;
-                case 4:
-                    meses.put("Abril", meses.get("Abril") + 1);
-                    break;
-                case 5:
-                    meses.put("Mayo", meses.get("Mayo") + 1);
-                    break;
-                case 6:
-                    meses.put("Junio", meses.get("Junio") + 1);
-                    break;
-                case 7:
-                    meses.put("Julio", meses.get("Julio") + 1);
-                    break;
-                case 8:
-                    meses.put("Agosto", meses.get("Agosto") + 1);
-                    break;
-                case 9:
-                    meses.put("Septiembre", meses.get("Septiembre") + 1);
-                    break;
-                case 10:
-                    meses.put("Octubre", meses.get("Octubre") + 1);
-                    break;
-                case 11:
-                    meses.put("Noviembre", meses.get("Noviembre") + 1);
-                    break;
-                case 12:
-                    meses.put("Diciembre", meses.get("Diciembre") + 1);
-                    break;
-
-            }
+        for (String nombre:mesesNombre){
+            Map<String ,Object> mes=new HashMap<String,Object>();
+            mes.put("Mes",nombre);
+            mes.put("Data",0);
+            data.add(mes);
         }
 
-        return meses;
+
+        for (Pedido pedidoEvaluar:lista){
+            int num = pedidoEvaluar.getFechaPedido().getMonthValue();
+            Map<String ,Object> mes = data.get(num-1);
+            mes.put("Data", (int) mes.get("Data") + 1);
+        }
+
+        return data;
     }
 }
