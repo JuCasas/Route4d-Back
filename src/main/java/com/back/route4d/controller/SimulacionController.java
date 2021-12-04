e77e9c4c26361d87b9a74af406c14af61a6cc060
Juan <jcasasz@pucp.edu.pe>
2021-12-03 19:06:41 -0500

    Controller Bloqueos Simulacion

--- src/main/java/com/back/route4d/controller/SimulacionController.java
+++ src/main/java/com/back/route4d/controller/SimulacionController.java
@@ -1,6 +1,7 @@
 package com.back.route4d.controller;
 
 import com.back.route4d.algoritmo.Simulacion;
+import com.back.route4d.model.CallesBloqueadas;
 import com.back.route4d.model.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,5 +28,16 @@ public class SimulacionController {
     public ResponseEntity<List<Pedido>> getPedidos(){
         return new ResponseEntity<>(simulacion.getPedidos(), HttpStatus.OK);
     }
+
+    @PostMapping(value = "/uploadCallesBloqueadas")
+    public ResponseEntity<String> uploadCallesBloqueadas(@RequestParam(value = "file") MultipartFile file){
+        return new ResponseEntity<>(simulacion.subirArchivoCallesBloqueadas(file), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/getListaCallesBloqueadas")
+    public ResponseEntity<List<CallesBloqueadas>> getListaCallesBloqueadas(){
+        return new ResponseEntity<>(simulacion.getListaCallesBloqueadas(), HttpStatus.OK);
+    }
+
 
 }
