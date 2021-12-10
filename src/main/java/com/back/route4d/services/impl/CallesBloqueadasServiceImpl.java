46934d30f91c49ab538a35e259245b1d7e5ab40c
Rodrigo Alonso <52440153+RodCross@users.noreply.github.com>
2021-12-09 21:07:33 -0500

    Bloqueos del día 2

--- src/main/java/com/back/route4d/services/impl/CallesBloqueadasServiceImpl.java
+++ src/main/java/com/back/route4d/services/impl/CallesBloqueadasServiceImpl.java
@@ -8,6 +8,7 @@ import com.back.route4d.services.CallesBloqueadasService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -27,33 +28,55 @@ public class CallesBloqueadasServiceImpl implements CallesBloqueadasService {
         List<CallesBloqueadas> listaCallesBloqueadas = callesBloqueadasRepository.findAll();
         List<CallesBloqueadasFront> listaCallesBloqueadasFront = new ArrayList<>();
 
+        // para el día de hoy
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime tomorrow = today.plusDays(1);
+
+        int yearToday = today.getYear();
+        Month monthToday = today.getMonth();
+        int dayToday = today.getDayOfMonth();
+
+        int yearTomorrow = tomorrow.getYear();
+        Month monthTomorrow = tomorrow.getMonth();
+        int dayTomorrow = tomorrow.getDayOfMonth();
+
+        LocalDateTime start = LocalDateTime.of(yearToday, monthToday, dayToday, 0, 0);
+        LocalDateTime finish = LocalDateTime.of(yearTomorrow, monthTomorrow, dayTomorrow, 0, 0);
+
         for (CallesBloqueadas callesBloqueadas : listaCallesBloqueadas) {
             Integer minutosInicio = callesBloqueadas.getMinutosInicio();
             Integer minutosFin = callesBloqueadas.getMinutosFin();
             LocalDateTime fechaInicio = Helper.convertMinutesToLocalDateTime(minutosInicio);
             LocalDateTime fechaFin = Helper.convertMinutesToLocalDateTime(minutosFin);
-            String nodos = callesBloqueadas.getNodos();
-            List<String> listaNodos = new ArrayList<String>(Arrays.asList(nodos.split(",")));
 
-            for (String s : listaNodos) {
-                int nodo = Integer.valueOf(s);
+            // verificando si la fecha está en rango
+            boolean fechaInicioEnRango = fechaInicio.isAfter(start) && fechaInicio.isBefore(finish);
+            boolean fechaFinEnRango = fechaFin.isAfter(start) && fechaFin.isBefore(finish);
+            boolean bloqueoEnRango = fechaInicio.isBefore(start) && fechaFin.isAfter(finish);
+            if (fechaInicioEnRango || fechaFinEnRango || bloqueoEnRango) {
+                String nodos = callesBloqueadas.getNodos();
+                List<String> listaNodos = new ArrayList<String>(Arrays.asList(nodos.split(",")));
 
-                int x = (nodo - 1) % 71;
-                int y = (nodo - 1) / 71;
+                for (String s : listaNodos) {
+                    int nodo = Integer.valueOf(s);
 
-                boolean encontrado = false;
-                for (CallesBloqueadasFront bloqueoRevisar : listaCallesBloqueadasFront) {
-                    if (bloqueoRevisar.esBloqueo(x,y)){
-                        bloqueoRevisar.addTime(fechaInicio,fechaFin);
-                        encontrado = true;
-                        break;
+                    int x = (nodo - 1) % 71;
+                    int y = (nodo - 1) / 71;
+
+                    boolean encontrado = false;
+                    for (CallesBloqueadasFront bloqueoRevisar : listaCallesBloqueadasFront) {
+                        if (bloqueoRevisar.esBloqueo(x,y)){
+                            bloqueoRevisar.addTime(fechaInicio,fechaFin);
+                            encontrado = true;
+                            break;
+                        }
                     }
-                }
 
-                if(!encontrado) {
-                    CallesBloqueadasFront calleFront = new CallesBloqueadasFront(x,y);
-                    calleFront.addTime(fechaInicio,fechaFin);
-                    listaCallesBloqueadasFront.add(calleFront);
+                    if(!encontrado) {
+                        CallesBloqueadasFront calleFront = new CallesBloqueadasFront(x,y);
+                        calleFront.addTime(fechaInicio,fechaFin);
+                        listaCallesBloqueadasFront.add(calleFront);
+                    }
                 }
             }
         }
